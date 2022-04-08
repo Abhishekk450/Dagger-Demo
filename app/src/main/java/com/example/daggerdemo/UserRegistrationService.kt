514f13bd0596c92ebecc791836e97a0b06920588
Abhishekk450 <abhishek@Abhisheks-MacBook-Pro.local>
2022-04-08 16:50:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/daggerdemo/UserRegistrationService.kt
@@ -0,0 +1,22 @@
+package com.example.daggerdemo
+
+class UserRegistrationService(private val userRepository: UserRepository,
+                              private val emailService: EmailService) {
+
+    fun registerUser(email:String, password:String) {
+        userRepository.saveUser(email, password)
+        emailService.send(email, "abc@gmail.com", "User Verified")
+    }
+}
+
+       /*
+        Unit Testing = means UserRepository and EmailService Class is ready than we use UserRegistration Class
+
+        Single Responsibilty = means it can work two task it create an object or it register the user also
+
+        Lifetime of objects = means User Repository and Email service attach with UserRegistrationService means if
+        UserRegistrationService is destroy it destroy both of the objects(not reuse)
+
+        Extensible = means it hard code the object in case in future we want to change the object we make a new object
+        rather than modify in existense one.
+        */
